8eac5d4806aea84136ea384cc9c129b2e2c8136e
nicolas.morbieu <nicolas.morbieu@gmail.com>
2014-10-15 21:11:59 +0200

    Service to serve big numbers

---
+++ BigNumberService/BigNumberService/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.Threading
+open Nancy
+open Nancy.Hosting.Self
+
+module BigNumberService =
+
+  let seed = 100000000
+
+  type BigNumberModule() as self =
+    inherit NancyModule()
+    do 
+      self.Get.["/givemeanumber/{id}"] <-
+        fun x ->
+          Thread.Sleep(1000)
+          let id = (((x :?> DynamicDictionary).Item "id") :?> DynamicDictionaryValue).Value :?> string |> int
+          sprintf "I choose %d" (seed + id) :> obj
+
+[<EntryPoint>]
+let main argv = 
+    let uri = "http://localhost:9006/"
+    let nancyHost = new NancyHost(new Uri(uri))
+    nancyHost.Start()
+    printfn "Service started..."
+    printfn "go to %s/givemeanumber/{n} where n is an integer to get a number" uri
+    printf "Press any key to stop"
+    Console.ReadKey() |> ignore
+    nancyHost.Stop()
+    0
